6b5cf1b5d0eba8e9e86827f8bf1e5c8b9fb7eca4
Harvey K <h.keown@af-sd.com>
2025-08-10 14:44:20 +0200

    Virgin commit

---
+++ claude_artifacts/files/sample_20250810_100638.py
@@ -0,0 +1,8 @@
+print("Hello from Claude Artifact Manager!")
+
+def welcome():
+    """Sample function to test the artifact system"""
+    return "Welcome to the robust artifact management system!"
+
+if __name__ == "__main__":
+    print(welcome())
